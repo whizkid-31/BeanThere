03845a3fcc4bfb3d91ad9535daa28e3755f7cfc4
whizkid-31 <aaryan31.gera@gmail.com>
2025-05-10 16:53:30 +0530

    Done with Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import HomePage from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Home";
+import ExplorePage from "./pages/Explore";
+import CafeDetailsPage from "./pages/CafeDetails";
+import BookmarksPage from "./pages/Bookmarks";
+import AboutPage from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <h1 style={{textAlign:"center", margin: "25px 0"}} >☕ Welcome to BeanThere</h1>
-      <HomePage />
-    </div>
+    <Router>
+      <Navbar />
+      <h1 style={{ textAlign: "center", margin: "25px 0" }}>☕ Welcome to BeanThere</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/cafe-details" element={<CafeDetailsPage />} />
+        <Route path="/bookmarks" element={<BookmarksPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo">BeanThere</div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/explore">Explore</Link></li>
+        <li><Link to="/cafe-details">Café Details</Link></li>
+        <li><Link to="/bookmarks">Bookmarks</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
